9b2e4f04e35bee165d56cfd0a9374fba60c9f2c3
Nour Atef <Nour Atef@LAPTOP-2M74GP4J>
2026-08-29 07:55:30 +0300

    Question 1

--- C#Basics-02.Answers/Program.cs
+++ C#Basics-02.Answers/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace C_Basics_02.Answers
+﻿using System.Runtime.InteropServices;
+
+namespace C_Basics_02.Answers
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            object book = new Book();
+            Console.WriteLine(book);
+
+           
+
+
+
             
+
+            
+
         }
+
+
+
     }
+
+
+
+
+    class Book
+    {
+        string Title;
+        int Pages;
+    }
+
+
 }
