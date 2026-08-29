bcadfc5e75eb0f72b527e3e8468354844371f6f1
Nour Atef <Nour Atef@LAPTOP-2M74GP4J>
2026-08-29 08:21:20 +0300

    Question 2

--- C#Basics-02.Answers/Program.cs
+++ C#Basics-02.Answers/Program.cs
@@ -9,21 +9,31 @@ namespace C_Basics_02.Answers
             object book = new Book();
             Console.WriteLine(book);
 
-           
+            string toString = book.ToString();
+
+            Console.WriteLine(toString);
+
+            bool compare = book.Equals(book);
+
+            Console.WriteLine(compare);
+
+            int hashCode = book.GetHashCode();
+
+            Console.WriteLine(hashCode);
+
+            Type type = book.GetType();
+
+            Console.WriteLine(type);
 
 
 
             
 
-            
+
 
         }
 
-
-
     }
-
-
 
 
     class Book
